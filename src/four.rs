8bdaa95cd3f83fc1b2c4b83a85abaf40262712da
Christopher Wells <cwellsny@nycap.rr.com>
2022-12-06 22:37:25 -0800

    Add tests for day4

--- src/four.rs
+++ src/four.rs
@@ -1,15 +1,15 @@
-use std::io;
 use std::io::prelude::*;
+use std::io::{self, BufReader};
 
 pub fn part_one() {
-    let values = read_input();
+    let values = read_input(&mut BufReader::new(io::stdin()));
     let answer = find_contained_ranges(&values);
 
     println!("{}", answer);
 }
 
 pub fn part_two() {
-    let numbers = read_input();
+    let numbers = read_input(&mut BufReader::new(io::stdin()));
     let answer = find_overlapping_ranges(&numbers);
 
     println!("{}", answer);
@@ -17,11 +17,9 @@ pub fn part_two() {
 
 type Range = (i32, i32);
 
-fn read_input() -> Vec<(Range, Range)> {
-    let stdin = io::stdin();
-
+fn read_input<T: std::io::Read>(reader: &mut BufReader<T>) -> Vec<(Range, Range)> {
     let mut numbers: Vec<((i32, i32), (i32, i32))> = Vec::new();
-    for line in stdin.lock().lines() {
+    for line in reader.lines() {
         let line = line.unwrap();
 
         let mut parts = line.split(',');
@@ -80,4 +78,56 @@ fn overlaps(a: &Range, b: &Range) -> bool {
     let (b_s, b_e) = b;
 
     a_s <= b_e && b_s <= a_e
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    use std::fs::File;
+
+    #[test]
+    fn test_part_1_example() {
+        let f = File::open("inputs/four_example.txt").unwrap();
+        let values = read_input(&mut BufReader::new(f));
+
+        let expected = 2;
+        let actual = find_contained_ranges(&values);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_part_1_actual() {
+        let f = File::open("inputs/four.txt").unwrap();
+        let values = read_input(&mut BufReader::new(f));
+
+        let expected = 485;
+        let actual = find_contained_ranges(&values);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_part_2_example() {
+        let f = File::open("inputs/four_example.txt").unwrap();
+        let values = read_input(&mut BufReader::new(f));
+
+        let expected = 4;
+        let actual = find_overlapping_ranges(&values);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_part_2_actual() {
+        let f = File::open("inputs/four.txt").unwrap();
+        let values = read_input(&mut BufReader::new(f));
+
+        let expected = 857;
+        let actual = find_overlapping_ranges(&values);
+
+        assert_eq!(expected, actual)
+    }
 }
